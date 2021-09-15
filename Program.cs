0d490013b65fadfea3a14cff0d3d3ae13a031327
Bevanje <56934698+Bevanje@users.noreply.github.com>
2021-09-14 21:08:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+
+namespace _3x_1
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+             int ThreeX(int value)
+            {
+                while(value != 1)
+                {
+                    if(value % 2 == 0)
+                    {
+                        value = value /2;
+                    }
+                    else
+                    {
+                        value = 3 * value + 1;
+                    }
+                }
+                return value;
+            }
+            Console.WriteLine(ThreeX(5));
+        }
+    }
+}
